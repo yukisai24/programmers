1fdf7755c8160b54f56e6bf0ffb313f3a6de9dd8
DT-YUKISAI\USER <yukisai24@naver.com>
2023-07-20 01:38:02 +0900

    7.20

--- defense.js
+++ defense.js
@@ -1,17 +1,25 @@
 function solution(n, k, enemy) {
-  var answer = 0;
-  const enemySum = [];
-  const kValue = [];
-  let tempSum = 0;
-  enemy.forEach((e) => {
-    tempSum += e;
-    enemySum.push(tempSum);
-  });
-  let notUsingK = enemySum.findIndex((e) => e < n);
-  //무적권 안쓰고 최대 갈 수 있는 스테이지
-  //그 때까지 제일 큰 수 부터 작은 수 까지 k개 나열
-  //제일 큰 수 뽑고 최대 갈 수 있는 스테이지에서 한칸ㄴㅇㄹㄴㅇㄹ
-  return enemySum;
+  const enemyLen = enemy.length; //총 enemy wave
+  if (k >= enemyLen) return enemyLen; //enemy wave 보다 무적권이 많은 경우 길이 return
+  let killedEnemy = 0;
+  const kValue = enemy.slice(0, k); //앞에서부터 k개 뽑기
+  for (let i = 0; i < enemyLen - k; i++) {
+    //k개 뽑은거에서 젤 작은 거랑 k+1 wave랑 비교
+    let minK = Math.min(...kValue);
+
+    if (enemy[i + k] > minK) {
+      kValue[kValue.indexOf(minK)] = enemy[i + k];
+      killedEnemy += minK;
+    } else {
+      killedEnemy += enemy[i + k];
+    }
+    if (killedEnemy > n) return i + k;
+  }
+  return enemyLen;
 }
 
-console.log(solution(7, 3, [4, 2, 4, 5, 3, 3, 1]));
+const arr = [
+  4, 2, 4, 5, 3, 3, 1, 10, 2, 5, 4, 6, 6, 5, 4, 7, 10, 40, 2, 34, 32, 14, 23, 6,
+];
+
+console.log(arr.slice(0, 0));
