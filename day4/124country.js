dea5503f6bde23ded7d7386322d75b99c13f6f80
yukisai24 <yukisai24@naver.com>
2023-07-28 12:39:44 +0900

    asdf

--- day4/124country.js
+++ day4/124country.js
@@ -1,15 +1,50 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/12899#qna
 function solution(n) {
-  var answer = "";
-  let m = n.toString(3);
-  console.log(m.replace("2", "4").replace("1", "2").replace("0", "1"));
-  m.replace("1", "2");
-  m.replace("0", "1");
-  console.log(m);
-  return answer;
+  const stack = [];
+  do {
+    if (n % 3 === 0) {
+      // 나머지가 0이면 나머지를 4로 바꿔주고 몫에서 -1.
+      stack.push(4);
+      n = Math.floor(n / 3) - 1;
+    } else {
+      // 나머지가 0이 아닌 경우 3진법으로 계산.
+      stack.push(n % 3);
+      n = Math.floor(n / 3);
+    }
+  } while (n > 0);
+  // n이 0보다 클때까지 반복. (n !== 0)는 유효성 5번 시간초과.
+
+  return stack.reverse().join("");
+  //stack = [1, 4] 이므로 뒤집어서 문자열로 변환 => return 값은 '41'
 }
-solution(10);
-const p = "as df gh";
-console.log(p.replace("a", "df"));
+
+solution(39);
+// const p = "as df gh";
+// console.log(p.replace("a", "df"));
 //11 42
 // 12 44
 // 13 111
+// 1 1 1
+// 2 2 2
+// 3 4 10
+
+// 4 11 11
+// 5 12 12
+// 6 14 20
+// 7 21 21
+// 8 22 22
+// 9 24 100
+// 10 41 101
+// 11 42 102
+// 12 44 110
+//  444444 1111110
+
+// 13 111 111
+// 14 112 112
+// 15 114 120
+
+// 00000
+// 00001
+// 00002
+// 00004
+// 00011
---
+++ day4/restaurant.js
@@ -0,0 +1,67 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/72411
+function solution(orders, course) {
+  var answer = [];
+  const alphaList = Object.keys(whichAlpha(orders));
+
+  return answer;
+}
+function maxOrder(orders, alphaList, num) {
+  let combinationList = combination(alphaList, num);
+  let countList = [];
+  let max = 0;
+  let filter = [];
+  combinationList.forEach((comb) => {
+    let combCount = howManyExist(orders, comb);
+    if (max < combCount) max = combCount;
+    countList.push(combCount);
+  });
+  countList.forEach((e, index) => {
+    if (e === max) filter.push(combinationList[index]);
+  });
+  return filter;
+}
+function combination(alphaList, num) {
+  let list = [];
+  if (num === 1) return alphaList;
+  else {
+    alphaList.forEach((alpha, index) => {
+      let suffix = combination(alphaList.slice(index + 1), num - 1);
+      for (let i = 0; i < suffix.length; i++) {
+        list.push([alpha, ...suffix[i]]);
+      }
+    });
+  }
+  return list;
+}
+function isExist(order, sample) {
+  return sample.every((e) => order.includes(e));
+}
+
+function howManyExist(orders, sample) {
+  let count = 0;
+  for (let i = 0; i < orders.length; i++) {
+    isExist(orders[i], sample) && count++;
+  }
+  return count;
+}
+
+function whichAlpha(orders) {
+  const allAlpha = {};
+  for (let i = 0; i < orders.length; i++) {
+    for (let j = 0; j < orders[i].length; j++) {
+      let alpha = orders[i][j];
+      allAlpha[alpha] = allAlpha[alpha] ? allAlpha[alpha] + 1 : 1;
+    }
+  }
+  return allAlpha;
+}
+
+// console.log(
+//   howManyExist(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], ["A", "C"])
+// )
+// console.log(["a", "c", "d", "b"]);
+// console.log(["a", "c", "d", "b"].sort());
+// console.log(
+//   Object.keys(whichAlpha(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]))
+// );
+console.log(combination(["A", "B", "C", "F", "G", "D", "E", "H"], 3));
