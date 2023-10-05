1844749d2be6622f5129974d2c8676a0077ab417
yukisai24 <yukisai24@naver.com>
2023-10-05 13:13:09 +0900

    ㄴㄴ

--- day16/땅따먹기.js
+++ day16/땅따먹기.js
@@ -1,22 +1,16 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/12913
 //5점
 function solution(land) {
-  let landCopy = [];
-  land.forEach((e) => landCopy.push(e));
   for (let i = 1; i < land.length; i++) {
-    landCopy[i][0] =
-      landCopy[i][0] +
-      Math.max(landCopy[i - 1][1], landCopy[i - 1][2], landCopy[i - 1][3]);
-    landCopy[i][1] =
-      landCopy[i][1] +
-      Math.max(landCopy[i - 1][0], landCopy[i - 1][2], landCopy[i - 1][3]);
-    landCopy[i][2] =
-      landCopy[i][2] +
-      Math.max(landCopy[i - 1][0], landCopy[i - 1][1], landCopy[i - 1][3]);
-    landCopy[i][3] =
-      landCopy[i][3] +
-      Math.max(landCopy[i - 1][0], landCopy[i - 1][1], landCopy[i - 1][2]);
+    land[i][0] =
+      land[i][0] + Math.max(land[i - 1][1], land[i - 1][2], land[i - 1][3]);
+    land[i][1] =
+      land[i][1] + Math.max(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+    land[i][2] =
+      land[i][2] + Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+    land[i][3] =
+      land[i][3] + Math.max(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
   }
 
-  return Math.max(...landCopy.at(-1));
+  return Math.max(...land.at(-1));
 }
